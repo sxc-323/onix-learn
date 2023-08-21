247be6eb725fcbeaecffdf905e84a5ee5ec0c8e0
sxc <sxc323@gmail.com>
2023-08-21 22:51:01 +0800

    clean master.img...

--- src/boot.asm
+++ src/boot.asm
@@ -11,26 +11,21 @@ mov es,ax
 mov ss,ax
 mov sp,0x7c00
 
-
-
-
 mov si,booting
 call print
 
-xchg bx,bx;
-
 mov edi,0x1000; 读取的目标内存
-mov ecx,0; 起始扇区
-mov bl,1; 扇区数量
+mov ecx,2; 起始扇区
+mov bl,4; 扇区数量
 
 call read_disk
 
-mov edi,0x1000
-mov ecx,1
-mov bl,1
-;call write_disk
+xchg bx,bx;
 
-jmp $
+cmp word [0x1000],0x55aa
+jnz error
+
+jmp 0:0x1002
 
 read_disk:
     mov dx, 0x1f2
@@ -86,7 +81,7 @@ read_disk:
             jnz .check
         ret
     
-    .reads
+    .reads:
         mov dx,0x1f0
         mov cx,256
         .readw:
@@ -97,73 +92,6 @@ read_disk:
             mov [edi],ax
             add edi,2
             loop .readw
-        ret
-
-write_disk:
-    mov dx, 0x1f2
-    mov al,bl
-    out dx,al
-
-    inc dx  ;0x1f3
-    mov al,cl
-    out dx,al
-
-    inc dx  ;0x1f4
-    shr ecx,8
-    mov al,cl
-    out dx,al
-
-    inc dx  ;0x1f5
-    shr ecx,8
-    mov al,cl
-    out dx,al
-
-    inc dx  ;0x1f6
-    shr ecx,8
-    and cl,0b1111;将高四位设为0
-
-    or cl,0b1110_0000
-    mov al,cl
-    out dx,al;主盘 LBA模式
-
-    inc dx  
-    mov al,0x30
-    out dx,al
-
-    xor ecx,ecx
-    mov cl,bl; 得到读取扇区的数量
-
-    .write:
-        push cx
-        call .writes
-        call .waits
-        pop cx
-        loop .write
-    ret
-
-    .waits:
-        mov dx,0x1f7
-        .check:
-            in al,dx
-            jmp $+2
-            jmp $+2
-            jmp $+2
-            and al,0b1000_0000
-            cmp al,0b0000_0000
-            jnz .check
-        ret
-    
-    .writes
-        mov dx,0x1f0
-        mov cx,256
-        .writew:
-            mov ax,[edi]
-            out dx,ax
-            jmp $+2
-            jmp $+2
-            jmp $+2
-            add edi,2
-            loop .writew
         ret
 
 print:
@@ -180,6 +108,13 @@ print:
 
 booting:
     db "Booting Onix...",10,13,0 ;\n\r
+
+error:
+    mov si,.msg
+    call print
+    hlt ;让CPU停止
+    jmp $
+    .msg db "Booting error...",10,13,0 ;\n\r
 
 times 510-($-$$) db 0
 
