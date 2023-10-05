c4cc0a5d6285fee61151d6ed635cd1e1dd599639
sxc <sxc323@gmail.com>
2023-10-05 10:50:39 +0800

    018 可变参数原理

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,19 +3,26 @@
 #include <onix/io.h>
 #include <onix/string.h>
 #include <onix/console.h>
+#include <onix/stdarg.h>
 
-char message[]="hello onix!!!\n";
-char buf[1024];
+void test_args(int cnt, ...)
+{
+    va_list args;
+    va_start(args,cnt);
+
+    int arg;
+    while (cnt--)
+    {
+        arg=va_arg(args,int);
+    }
+    va_end(args);    
+}
 
 void kernel_init()
 {
     console_init();
-
-    u32 count =120;
-    while (true)
-    {
-        console_write(message,sizeof(message)-1);
-    }
+    test_args(5,1,0xaa,5,0x55,10);
+    
     
     
     return;
