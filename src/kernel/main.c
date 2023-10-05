dc6e078917631a90febaffeb4a82ffc8d2616a2e
sxc <sxc323@gmail.com>
2023-10-05 10:51:26 +0800

    019 printk

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -3,27 +3,16 @@
 #include <onix/io.h>
 #include <onix/string.h>
 #include <onix/console.h>
-#include <onix/stdarg.h>
-
-void test_args(int cnt, ...)
-{
-    va_list args;
-    va_start(args,cnt);
-
-    int arg;
-    while (cnt--)
-    {
-        arg=va_arg(args,int);
-    }
-    va_end(args);    
-}
+#include <onix/printk.h>
 
 void kernel_init()
 {
     console_init();
-    test_args(5,1,0xaa,5,0x55,10);
-    
-    
+    int cnt=50;
+    while (cnt--)
+    {
+        printk("hello onix %#010x,%d\n",cnt,cnt);
+    }
     
     return;
 }
