0b8b49529e15e720c12e0620a45c7e3c7539ebfd
sxc <sxc323@gmail.com>
2023-10-10 22:07:02 +0800

    022 内核GDT

--- src/include/onix/debug.h
+++ src/include/onix/debug.h
@@ -4,6 +4,6 @@
 void debugk(char *file,int line ,const char * fmt,...);
 
 #define BMB asm volatile("xchgw %bx, %bx") //bochs magic breakpoint
-#define DEBUGK(fmt,args...) debugk(__BASE_FILE__,__FILE__,fmt, ##args)
+#define DEBUGK(fmt,args...) debugk(__BASE_FILE__,__LINE__,fmt, ##args)
 
 #endif
