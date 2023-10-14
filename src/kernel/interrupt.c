6dde04fbf1c39024cc9bbc2e5a323a5bca921d04
sxc <sxc323@gmail.com>
2023-10-14 20:18:00 +0800

    024 中断函数 025 中断描述符

---
+++ src/kernel/interrupt.c
@@ -0,0 +1,28 @@
+#include <onix/interrupt.h>
+#include <onix/global.h>
+#include <onix/debug.h>
+
+gate_t idt[IDT_SIZE];
+pointer_t idt_ptr;
+
+extern void interrupt_handler();
+
+void interrupt_init()
+{
+    for (size_t i = 0; i < IDT_SIZE; i++)
+    {
+        gate_t *gate=&idt[i];
+        gate->offset0 =(u32)interrupt_handler & 0xffff;
+        gate->offset1 =((u32)interrupt_handler >> 16)& 0xffff;
+        gate->selector = 1<<3;
+        gate->reserved=0;
+        gate-> type=0b1110;
+        gate->segment=0;
+        gate->DPL=0;
+        gate->present=1;
+    }
+    idt_ptr.base=(u32)idt;
+    idt_ptr.limit=sizeof(idt)-1;
+    BMB;
+    asm volatile("lidt idt_ptr\n");
+}
--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -6,5 +6,6 @@ global _start
 _start:
     
     call kernel_init
-    ; mov byte [0xb8000],'k'
+    xchg bx,bx
+    int 0x80; 调用 0x80 中断函数 系统调用
     jmp $
