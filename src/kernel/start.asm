c6c5a4395273465fb535307585e9315d427f59f8
sxc <sxc323@gmail.com>
2023-11-18 22:39:26 +0800

    035 物理内存管理

--- src/kernel/start.asm
+++ src/kernel/start.asm
@@ -1,6 +1,7 @@
 [bits 32]
 
 extern console_init
+extern gdt_init
 extern memory_init
 extern kernel_init 
 
@@ -10,6 +11,8 @@ _start:
     push eax; magic
 
     call console_init; 控制台初始化
+    call gdt_init   ; 全局描述符初始化
     call memory_init; 内存初始化
+    call kernel_init; 内核初始化
     
     jmp $
